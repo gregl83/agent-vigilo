e355eb4ff3c307ffe804fc3e6a9d1b04306df496
gregory langlais <_@gregorylanglais.com>
2026-04-22 19:47:03 -0700

    add tracing to cli app

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
+use std::io::stderr;
+
 use clap::Parser;
 
 mod cli;
-
 use cli::{
     App,
     Executable,
 };
+use tracing::{
+    error,
+    Level,
+};
+use tracing_subscriber::{
+    EnvFilter,
+    fmt,
+    prelude::*,
+};
+
+
+fn init_logger(quiet: bool, verbose: u8) {
+    let level = if quiet {
+        Level::ERROR
+    } else {
+        match verbose {
+            0 => Level::INFO,
+            1 => Level::DEBUG,
+            _ => Level::TRACE,
+        }
+    };
+
+    tracing_subscriber::registry()
+        .with(
+            fmt::layer()
+                .with_writer(stderr)
+                .with_target(false)
+        )
+        .with(
+            EnvFilter::from_default_env()
+                .add_directive(level.into())
+        )
+        .init();
+}
 
 #[tokio::main]
 async fn main() {
-    // todo - bootstrap logger/output
-
     let cli = App::parse();
-    let result = cli.exec().await;
 
-    // todo - do something with result
+    init_logger(cli.quiet, cli.verbose);
+
+    match cli.exec().await {
+        Ok(_) => (),
+        Err(e) => {
+            error!("{}", e);
+        },
+    }
 }
