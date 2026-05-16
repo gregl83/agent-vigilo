73a2765d3cb03ade4096f5318ff44b5756298d61
gregory langlais <_@gregorylanglais.com>
2026-05-15 22:08:12 -0700

    remove blog from website

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -50,21 +50,7 @@ const config: Config = {
           editUrl:
             'https://github.com/gregl83/agent-vigilo/tree/main/docs/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/docs/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -113,7 +99,6 @@ const config: Config = {
           position: 'left',
           label: 'Guides',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/gregl83/agent-vigilo',
           label: 'GitHub',
@@ -149,10 +134,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/gregl83/agent-vigilo',
